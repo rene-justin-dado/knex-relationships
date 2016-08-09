20adf703d1fc8df4e841d16ef4192c49b7b6e471
EDA <eda@mac-mini-3.local>
2016-08-09 13:46:41 +1200

    client-side rendering

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 var wombleList = require('./templates/womble-list.hbs')
+var headerTemplate = require('./templates/header.hbs')
+var footerTemplate = require('./templates/footer.hbs')
 
 document.addEventListener('DOMContentLoaded', function () {
+  var headerElement = document.getElementById('header')
+  var headerHTML = headerTemplate({
+    title: 'Wombles',
+    subtitle: 'They clean up'
+  })
+  headerElement.innerHTML = headerHTML
+
+
   var div = document.createElement('div')
   var data = {
     wombles: [
-      { name: 'Orinocc', email: 'orinocco@wimbledoncommon.net' },
-      { name: 'Toms', email: 'tomsk@wimbledoncommon.net' },
-      { name: 'Bung', email: 'bungo@wimbledoncommon.net' }
+      { name: 'Orinocco', email: 'orinocco@wimbledoncommon.net' },
+      { name: 'Tomsk', email: 'tomsk@wimbledoncommon.net' },
+      { name: 'Bungo', email: 'bungo@wimbledoncommon.net' }
     ]
   }
   div.innerHTML = wombleList(data)
   document.body.appendChild(div)
+
+  var footerElement = document.getElementById('footer')
+  var footerHTML = footerTemplate({
+    copyright: '&copy',
+    contact: '555-3000'
+  })
+  footerElement.innerHTML = footerHTML
 })
